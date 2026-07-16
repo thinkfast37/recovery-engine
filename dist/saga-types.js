5f9b41f295a5991aab4bec37b3eb380735e2af8d
Steve Sanyal <steve.sanyal@gmail.com>
2026-07-16 00:54:30 -0400

    Add .gitignore, stop tracking node_modules and dist

--- dist/saga-types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// FIXED CONTRACT — do not modify.
-// The orchestrator implementation must satisfy these interfaces exactly.
-Object.defineProperty(exports, "__esModule", { value: true });
